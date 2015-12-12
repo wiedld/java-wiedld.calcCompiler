d988aeee51c5ece644c690612e8e429ccbd74cbc
wiedld <dlw405@gmail.com>
2015-12-12 14:21:00 -0800

    minor cleanup

--- src/wiedld/calccompiler/LR0Parser.java
+++ src/wiedld/calccompiler/LR0Parser.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package wiedld.calccompiler;
 
-//import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.Stack;
